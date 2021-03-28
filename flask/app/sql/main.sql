be1fdbe8dcc0fbde443bacaeed090d807bfd36fc
piyush7gupta <piyush37gupta@gmail.com>
2021-03-28 10:13:48 +0530

    Changed main.sql file

--- flask/app/sql/main.sql
+++ flask/app/sql/main.sql
@@ -16,6 +16,11 @@ sum(Total_Individuals_Vaccinated)over(partition by state_id order by date_1 rows
 from vaccine_daily
 order by state_id,date_1;
 
+Create Materialized View India_population as 
+Select sum(Population) as Population 
+from
+state_and_ut;
+
 Create Materialized View India_Vaccine_Cumulative as
 Select date_1,
 sum(cum_Total_Individuals_Registered) as cum_Total_Individuals_Registered,
@@ -60,17 +65,44 @@ where district_id=366
 order by District_id,date_1;
 
 Create Materialized View India_Cumulative as
-Select date_1,sum(cum_Confirmed) as cum_Confirmed,sum(cum_Recovered) as cum_Recovered,sum(cum_Deceased) as cum_Deceased, sum(cum_Other) as cum_Other, sum(Active) as active
+Select date_1,sum(cum_Confirmed) as cum_Confirmed,sum(cum_Recovered) as cum_Recovered,sum(cum_Deceased) as cum_Deceased, sum(cum_Other) as cum_Other,sum(cum_Tested) as cum_Tested, sum(Active) as active
 from State_Cumulative 
+group by date_1
+order by date_1;
+
+Create Materialized view India_Daily as 
+Select *,confirmed-Recovered-other-Deceased as active from(
+Select date_1,sum(confirmed) as confirmed,sum(Recovered) as Recovered, sum(Deceased) as Deceased, sum(Other) as Other, sum(Tested) as Tested
+from state_daily
+group by date_1)
+as temp1
+order by date_1;
+
+Create Materialized view India_Vaccine_daily as
+Select date_1,
+sum(Total_Individuals_Registered) as Total_Individuals_Registered,
+sum(Total_Sessions_Conducted) as Total_Sessions_Conducted,
+sum(Total_Sites) as Total_Sites,
+sum(First_Dose_Administered) as First_Dose_Administered,
+sum(Second_Dose_Administered) as Second_Dose_Administered,
+sum(Male_Individuals_Vaccinated) as Male_Individuals_Vaccinated,
+sum(Female_Individuals_Vaccinated) as Female_Individuals_Vaccinated,
+sum(Transgender_Individuals_Vaccinated) as Transgender_Individuals_Vaccinated,
+sum(Total_Covaxin_Administered) as Total_Covaxin_Administered,
+sum(Total_CoviShield_Administered) as Total_CoviShield_Administered,
+sum(Total_Doses_Administered) as Total_Doses_Administered,
+sum(Total_Individuals_Vaccinated) as Total_Individuals_Vaccinated
+from vaccine_daily
 group by date_1
 order by date_1;
 
 
 
-
-
-Drop Materialized View India_Cumulative;
-Drop Materialized View India_Vaccine_Cumulative;
-Drop materialized view State_Vaccine_Cumulative;
-Drop materialized view District_Cumulative;
-Drop materialized view State_Cumulative;
+-- Drop Materialized view India_Vaccine_daily;
+-- Drop Materialized view India_Daily;
+-- Drop Materialized View India_Cumulative;
+-- Drop Materialized View India_Vaccine_Cumulative;
+-- Drop materialized view State_Vaccine_Cumulative;
+-- Drop materialized view District_Cumulative;
+-- Drop materialized view State_Cumulative;
+-- Drop Materialized View India_population;
