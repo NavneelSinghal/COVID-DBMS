ab6b7c837f7bbd5eba4bc7456e0cbf4d12d25051
sarthak <sarthakagrawal8@gmail.com>
2021-03-31 15:44:41 +0530

    states frontend apis

--- flask/app/static/js/states.js
+++ flask/app/static/js/states.js
@@ -107,19 +107,171 @@ function changeState() {
 	span.textContent = state_select.state.value;
   });
   state_select.stateid = state_ids[state_select.state.selectedIndex];
-
-  refreshSummary();
-  refreshDaily();
-  refreshVaccinations();
-  refreshDistricts();
-  refreshAnalysis();
+  refreshAll();
 }
 
-function refreshSummary() {}
-function refreshDaily() {}
-function refreshVaccinations() {}
-function refreshDistricts() {}
-function refreshAnalysis() {}
+function refreshSummary() {
+  let selectedCols = Array();
+  summary.columns.forEach((checkbox) => {
+	if (checkbox.checked) {
+	  selectedCols.push(checkbox.value);
+	}
+  });
+  if (selectedCols.length === 0) {
+	window.alert('Please select atleast one column');
+	return;
+  }
+
+  fetchget('/api/states/summary', {
+    stateid: state_select.stateid,
+	from: summary.from.value,
+	to: summary.to.value
+  }).then(response => response.json()
+  ).then(data => {
+	console.log(data);
+	//Clear existing table header and add new
+	
+	let header = document.createElement('tr');
+	selectedCols.forEach((column) => {
+	  let th = document.createElement('th');
+	  th.textContent = column;
+	  header.append(th);
+	});
+	summary.tableheader.replaceChildren(header);
+
+	let values = document.createElement('tr');
+	selectedCols.forEach((column) => {
+	  let td = document.createElement('td');
+	  td.textContent = data[column][0];
+	  values.append(td);
+	});
+	summary.tablebody.replaceChildren(values);
+  });
+}
+
+function refreshDaily() {
+  let type = daily.type.value;
+  let param = daily.parameter.value;
+  let ndays = daily.ndays.value;
+
+  fetchget('/api/states/daily', {
+    stateid: state_select.stateid,
+	type: type,
+	parameter: param,
+	ndays: ndays
+  })
+  .then(response => response.json())
+  .then(data => {
+	/* Refresh chart */
+	console.log(data);
+	daily.chart.data.datasets[0].label = param;
+	daily.chart.data.labels = data['Date'];
+	daily.chart.data.datasets[0].data = data[param];
+	daily.chart.update();
+  });
+}
+
+function refreshVaccinations() {
+  let selectedCols = Array();
+  vaccinations.columns.forEach((checkbox) => {
+	if (checkbox.checked) {
+	  selectedCols.push(checkbox.value);
+	}
+  });
+  if (selectedCols.length === 0) {
+	window.alert('Please select atleast one column');
+	return;
+  }
+
+  fetchget('/api/states/vaccine', {
+    stateid: state_select.stateid,
+	from: vaccinations.from.value,
+	to: vaccinations.to.value
+  }).then(
+	response => response.json()
+  ).then(
+	data => {
+	  //Clear existing table header and add new
+	  console.log(data);
+	  let header = document.createElement('tr');
+	  selectedCols.forEach((column) => {
+		let th = document.createElement('th');
+		th.textContent = column;
+		header.append(th);
+	  });
+	  vaccinations.tableheader.replaceChildren(header);
+
+	  let values = document.createElement('tr');
+	  selectedCols.forEach((column) => {
+		let td = document.createElement('td');
+		td.textContent = data[column][0];
+		values.append(td);
+	  });
+	  vaccinations.tablebody.replaceChildren(values);
+	}
+  );
+}
+
+function refreshDistricts() {
+  let param = districts.parameter.value;
+  let order = districts.order.value;
+
+  /* We will make api call here, for now assume dummy values */
+
+  fetchget('/api/states/listdistricts', {
+    stateid: state_select.stateid,
+	sortedby: param,
+	sortedin: order
+  }).then(
+	response => response.json()
+  ).then(
+	data => {
+	  console.log(data);
+	  let rows = Array();
+	  for (let i=0; i<data['Name'].length; i++) {
+		let row = document.createElement('tr');
+		for (let j=0; j<districts.tableheader.children.length; j++) {
+		  let column = districts.tableheader.children[j].textContent;
+		  let td = document.createElement('td');
+		  td.textContent = data[column][i];
+		  row.append(td);
+		}
+		rows.push(row);
+	  }
+	  districts.tablebody.replaceChildren(...rows);
+	}
+  );
+}
+
+function refreshAnalysis() {
+  fetchget('/api/states/analysis', {
+    stateid: state_select.stateid,
+	from: analysis.from.value,
+	to: analysis.to.value,
+	type: analysis.type.value,
+	parameter: analysis.parameter.value,
+	query: analysis.query.value
+  }).then(
+	response => response.json()
+  ).then(
+	data => {
+	  console.log(data);
+
+	  let rows = Array();
+	  for (let i=0; i<data['Date'].length; i++) {
+		let tr = document.createElement('tr');
+		let td1 = document.createElement('td');
+		let td2 = document.createElement('td');
+		td1.textContent = data['Date'][i];
+		td2.textContent = data[analysis.parameter.value][i];
+		tr.replaceChildren(td1, td2);
+		rows.push(tr);
+	  }
+	  analysis.tablebody.replaceChildren(...rows);
+	}
+  );
+}
+
 function validateAnalysisParameter(event) {
   let val = event.target.value;
   if (val === 'Cumulative') {
@@ -129,6 +281,14 @@ function validateAnalysisParameter(event) {
 	analysis.optgroups.daily.disabled = false;
 	analysis.optgroups.cumulative.disabled = true;
   }
+}
+
+function refreshAll() {
+  refreshSummary();
+  refreshDaily();
+  refreshVaccinations();
+  refreshDistricts();
+  refreshAnalysis();
 }
 
 fetchget('/api/states/list', {})
@@ -141,10 +301,19 @@ fetchget('/api/states/list', {})
 	window.alert('Unable to fetch list of states!');
 	return;
   }
-  data.states.forEach((state) => {
+  console.log(data);
+  for (let i=0; i<data['state'].length; i++) {
 	let opt = document.createElement('option');
-	opt.textContent = state.name;
+	opt.textContent = data['state'][i];
 	state_select.state.append(opt);
-	state_ids.push(state.id);
-  });
+	state_ids.push(data['state_id'][i]);
+  }
+
+  let url = new URL(window.location.href);
+  if (url.searchParams.has('stateid')) {
+    state_select.state.selectedIndex = url.searchParams.get('stateid');
+  } else {
+    state_select.state.selectedIndex = 0;
+  }
+  changeState();
 });
